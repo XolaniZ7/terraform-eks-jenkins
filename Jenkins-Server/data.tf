73738c334ddc923a30cbcf15e31fc1700ef78709
XolaniZ2 <xolani.zulu@disraptor.co.za>
2024-06-24 08:48:25 +0200

    updated

--- Jenkins-Server/data.tf
+++ Jenkins-Server/data.tf
@@ -1,24 +1,24 @@
 # Data block to fetch the most recent Amazon Linux 2 AMI
 data "aws_ami" "example" {
-  most_recent = true         # Ensures the most recent AMI is fetched
-  owners      = ["amazon"]   # AMIs owned by Amazon
+  most_recent = true       # Ensures the most recent AMI is fetched
+  owners      = ["amazon"] # AMIs owned by Amazon
 
-# Filters AMIs by name pattern
+  # Filters AMIs by name pattern
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]  
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 
-# Filters AMIs that use EBS as the root device type
+  # Filters AMIs that use EBS as the root device type
   filter {
     name   = "root-device-type"
-    values = ["ebs"]          
+    values = ["ebs"]
   }
 
-# Filters AMIs that use hardware virtualization
+  # Filters AMIs that use hardware virtualization
   filter {
     name   = "virtualization-type"
-    values = ["hvm"]          
+    values = ["hvm"]
   }
 }
 
--- Jenkins-Server/main.tf
+++ Jenkins-Server/main.tf
@@ -2,22 +2,22 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "jenkins-vpc"                # Name of the VPC
-  cidr = var.vpc_cidr                 # CIDR block for the VPC
+  name = "jenkins-vpc" # Name of the VPC
+  cidr = var.vpc_cidr  # CIDR block for the VPC
 
-  azs            = data.aws_availability_zones.available.names  # Availability zones for the subnets
-  public_subnets = var.public_subnets  # CIDR blocks for the public subnets
+  azs            = data.aws_availability_zones.available.names # Availability zones for the subnets
+  public_subnets = var.public_subnets                          # CIDR blocks for the public subnets
 
-  enable_dns_hostnames = true         # Enable DNS hostnames in the VPC
+  enable_dns_hostnames = true # Enable DNS hostnames in the VPC
 
   tags = {
-    name        = "jenkins-vpc"       # Name tag for the VPC
-    Terraform   = "true"              # Tag indicating it's managed by Terraform
-    Environment = "dev"               # Environment tag
+    name        = "jenkins-vpc" # Name tag for the VPC
+    Terraform   = "true"        # Tag indicating it's managed by Terraform
+    Environment = "dev"         # Environment tag
   }
 
   public_subnet_tags = {
-    name = "jenkins subnet"           # Name tag for the public subnets
+    name = "jenkins subnet" # Name tag for the public subnets
   }
 }
 
@@ -25,24 +25,24 @@ module "vpc" {
 module "vote_service_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "jenkins-sg"          # Name of the security group
-  description = "Security group for Jenkins"  # Description of the security group
-  vpc_id      = module.vpc.vpc_id     # VPC ID for the security group
+  name        = "jenkins-sg"                 # Name of the security group
+  description = "Security group for Jenkins" # Description of the security group
+  vpc_id      = module.vpc.vpc_id            # VPC ID for the security group
 
   ingress_with_cidr_blocks = [
     {
       from_port   = 8080
       to_port     = 8080
       protocol    = "tcp"
-      description = "HTTP"            # Allow HTTP traffic on port 8080
-      cidr_blocks = "0.0.0.0/0"       # Source CIDR block
+      description = "HTTP"      # Allow HTTP traffic on port 8080
+      cidr_blocks = "0.0.0.0/0" # Source CIDR block
     },
     {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
-      description = "SSH"             # Allow SSH traffic on port 22
-      cidr_blocks = "0.0.0.0/0"       # Source CIDR block
+      description = "SSH"       # Allow SSH traffic on port 22
+      cidr_blocks = "0.0.0.0/0" # Source CIDR block
     },
   ]
 
@@ -50,13 +50,13 @@ module "vote_service_sg" {
     {
       from_port   = 0
       to_port     = 0
-      protocol    = "-1"              # Allow all outbound traffic
-      cidr_blocks = "0.0.0.0/0"       # Destination CIDR block
+      protocol    = "-1"        # Allow all outbound traffic
+      cidr_blocks = "0.0.0.0/0" # Destination CIDR block
     }
   ]
 
   tags = {
-    name = "jenkins-sg"               # Name tag for the security group
+    name = "jenkins-sg" # Name tag for the security group
   }
 }
 
@@ -64,20 +64,20 @@ module "vote_service_sg" {
 module "ec2_instance" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "Jenkins-server"             # Name of the EC2 instance
+  name = "Jenkins-server" # Name of the EC2 instance
 
-  instance_type               = var.instance_type               # EC2 instance type
-  key_name                    = "jenkins-kp"                    # Key pair name for SSH access
-  monitoring                  = true                            # Enable detailed monitoring
-  vpc_security_group_ids      = [module.vote_service_sg.security_group_id]  # Security group for the instance
-  subnet_id                   = module.vpc.public_subnets[0]    # Subnet ID for the instance
-  associate_public_ip_address = true                            # Assign a public IP address
-  user_data                   = file("jenkins-install.sh")      # User data script for installing Jenkins
-  availability_zone           = data.aws_availability_zones.available.names[0]  # Availability zone for the instance
+  instance_type               = var.instance_type                              # EC2 instance type
+  key_name                    = "jenkins-kp"                                   # Key pair name for SSH access
+  monitoring                  = true                                           # Enable detailed monitoring
+  vpc_security_group_ids      = [module.vote_service_sg.security_group_id]     # Security group for the instance
+  subnet_id                   = module.vpc.public_subnets[0]                   # Subnet ID for the instance
+  associate_public_ip_address = true                                           # Assign a public IP address
+  user_data                   = file("jenkins-install.sh")                     # User data script for installing Jenkins
+  availability_zone           = data.aws_availability_zones.available.names[0] # Availability zone for the instance
 
   tags = {
-    name        = "jenkins-server"    # Name tag for the EC2 instance
-    Terraform   = "true"              # Tag indicating it's managed by Terraform
-    Environment = "dev"               # Environment tag
+    name        = "jenkins-server" # Name tag for the EC2 instance
+    Terraform   = "true"           # Tag indicating it's managed by Terraform
+    Environment = "dev"            # Environment tag
   }
 }
