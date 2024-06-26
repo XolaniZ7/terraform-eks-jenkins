ffa1dc83118540673d08befd724287c89fb3c3ec
XolaniZ2 <xolani.zulu@disraptor.co.za>
2024-06-26 13:33:29 +0200

    updated

--- Jenkins-Server/backend.tf
+++ Jenkins-Server/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks-767397690733"
+    bucket = "cicd-terraform-eks-975050055337"
     key    = "jenkins/terraform.tfstate"
     region = "us-east-1"
 
