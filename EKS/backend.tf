82c57189ea54d85685d67940467fee6e099f1bed
XolaniZ2 <xolani.zulu@disraptor.co.za>
2024-06-26 14:15:47 +0200

    updated

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks-533267054305"
+    bucket = "cicd-terraform-eks-851725635917"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
 
